82fe264e030808bb360bcdf848ecf3c8100ae56c
ozen-hub <info.ozenhub@gmail.com>
2023-05-28 22:57:24 +0530

    re created

--- src/main/java/com/devstack/app/dao/impl/CustomerDaoImpl.java
+++ src/main/java/com/devstack/app/dao/impl/CustomerDaoImpl.java
@@ -4,15 +4,15 @@ import com.devstack.app.dao.CustomerDao;
 import com.devstack.app.entity.Customer;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Queue;
 
-@Repository
+@Component
 public class CustomerDaoImpl implements CustomerDao {
 
     @Autowired
--- src/main/java/com/devstack/app/entity/Customer.java
+++ src/main/java/com/devstack/app/entity/Customer.java
@@ -1,10 +1,17 @@
 package com.devstack.app.entity;
 
-import jakarta.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "customer")

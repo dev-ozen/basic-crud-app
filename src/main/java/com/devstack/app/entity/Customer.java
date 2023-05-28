c7436f997304698a4593619e4ce2c23a6a99645c
ozen-hub <info.ozenhub@gmail.com>
2023-05-28 20:35:39 +0530

    6

---
+++ src/main/java/com/devstack/app/entity/Customer.java
@@ -0,0 +1,22 @@
+package com.devstack.app.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "customer")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+    private double salary;
+}
